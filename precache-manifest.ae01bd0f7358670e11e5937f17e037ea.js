c4c3d53d95f899e6076d1f8c7a24cfd96748f97e
Andrii Kicha <akicha@cogniance.com>
2018-12-05 18:01:51 +0200

    Including production build

---
+++ precache-manifest.ae01bd0f7358670e11e5937f17e037ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "85c83c8612892faeaeca",
+    "url": "/static/js/main.85c83c86.chunk.js"
+  },
+  {
+    "revision": "cb4a429b03ff91797975",
+    "url": "/static/js/1.cb4a429b.chunk.js"
+  },
+  {
+    "revision": "85c83c8612892faeaeca",
+    "url": "/static/css/main.6b5b4163.chunk.css"
+  },
+  {
+    "revision": "cb4a429b03ff91797975",
+    "url": "/static/css/1.66edfbb0.chunk.css"
+  },
+  {
+    "revision": "004d10dcb82a71a4dfdf5385c6667017",
+    "url": "/index.html"
+  }
+];
